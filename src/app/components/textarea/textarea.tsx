85f9605c36bd9f89063642766f9dcabe3b324af5
goshaGloss <georgiy.chetkov@gmail.com>
2024-08-17 18:16:57 +0500

    fix: ref typization

--- src/app/components/textarea/textarea.tsx
+++ src/app/components/textarea/textarea.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, TextareaHTMLAttributes } from "react";
+import { forwardRef, Ref, TextareaHTMLAttributes } from "react";
 import styles from "./index.module.css";
 import { Inter } from "next/font/google";
 
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const Textarea = forwardRef(function TextArea(
   props: TextareaHTMLAttributes<HTMLTextAreaElement>,
-  ref,
+  ref: Ref<HTMLTextAreaElement> ,
 ) {
   return (
     <textarea
